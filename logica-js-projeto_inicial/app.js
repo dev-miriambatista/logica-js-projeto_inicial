5983c797a867a1b48cf328cd656fd13021531ac5
Miriam Batista <dev.miriambatista@gmail.com>
2024-06-19 16:44:38 -0300

    EX JOGO SECRETO

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,5 +1,5 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 5;
+let numeroSecreto = 10;
 let chute = prompt("Escolha um número entre 1 e 10");
 
 if(chute == numeroSecreto){
