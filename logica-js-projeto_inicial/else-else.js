ab8d60b3b5a765db30da5a05982c88f2c4c0d3cf
Miriam Batista <dev.miriambatista@gmail.com>
2024-06-21 17:04:53 -0300

    while - enquanto

--- logica-js-projeto_inicial/else-else.js
+++ logica-js-projeto_inicial/else-else.js
@@ -3,38 +3,25 @@
 
 alert(`Boas vindas ao jogo do número secreto`);
 let numeroSecreto = 12;
-let chute = prompt(`Escolha um número entre 1 e 20`);
+let chute ;
+let tentativas  = 1
 
-chute = Number(chute);
 
-if (chute == numeroSecreto) {
-    alert(`Parabéns você acertou! O número correto é ${numeroSecreto}`);
-    //QUANDO UTILIZAR $, SEMPRE USAR CRASE NO LUGAR DE ASPAS 
-} else {
-    if (numeroSecreto < chute) {
-        chute = prompt(`O número é menor que ${chute} tente novamente`);
-    } else {
-        chute = prompt(`O número é maior que ${chute} tente novamente`);
-    }
-
+while (chute != numeroSecreto) {
+    //enquanto(while) o chute não for o numero correto, continuar(!) tentando.
 
     if (chute == numeroSecreto) {
-        alert(`Parabéns você acertou! O número correto é ${numeroSecreto}`);
+        alert(`Parabéns você acertou com ${tentativas} tentavivas! O número correto é ${numeroSecreto} `);
+        //QUANDO UTILIZAR $, SEMPRE USAR CRASE NO LUGAR DE ASPAS 
     } else {
         if (numeroSecreto < chute) {
             chute = prompt(`O número é menor que ${chute} tente novamente`);
         } else {
             chute = prompt(`O número é maior que ${chute} tente novamente`);
         }
-
-
         if (chute == numeroSecreto) {
-            alert(`Parabéns você acertou! O número correto é ${numeroSecreto}`);
-        } else {
-            alert(`não foi desta vez! O número correto é ${numeroSecreto}`);
-
-        }
+            alert(`Parabéns você acertou com ${tentativas} tentavivas! O número correto é ${numeroSecreto} `);
     }
+    tentativas++
 }
-
-
+}
